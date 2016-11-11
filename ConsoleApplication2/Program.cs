5ab4f1b40e1a3ef2a3b0fbd7f9132377475ec170
Diegos829 <django@163.com>
2016-11-11 20:09:24 +0800

    占位符和变量交换

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "猪大肠", 
+                phna = "弱鸡鸭",
+                phtype = "X78", 
+                price = "888";      
+            double weigh = 10.5;                 
+            char sex = '男';
+            int age = 28;
+            string ph = "13800138000";      
+            Console.WriteLine("此人叫做{0}，{1}的，{2}岁，帅的狗屎一样，电话号码是{3}。",name,sex,age,ph);
+            Console.WriteLine("电话型号是{0}{1}，这个手机卓越不凡，仅重{2}公斤，只卖{3}元。请到淘东网站，搜《猪大肠》，欲购从速。",phna,phtype,weigh,price);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3,
+                num2 = 9,
+                temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("num1={0},num2={1}。",num1,num2);
+            Console.ReadKey();
+
+        }
+    }
+}
