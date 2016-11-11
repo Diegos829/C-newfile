66b3bc2cb998443122956d510b12cade06670460
Diegos829 <django@163.com>
2016-11-11 20:22:52 +0800

    最基本的交互

---
+++ 输入交互/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 输入交互
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("可以键盘输入文字以便交互：");
+                     input = Console.ReadLine();
+            Console.WriteLine("你的问题是：{0}",input);
+            Console.ReadKey();
+        }
+    }
+}
