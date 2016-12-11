f40b376f31a37751e9f8e84b4263e3c5edd9b29b
Diegos829 <django@163.com>
2016-12-12 00:21:32 +0800

    bool2

---
+++ bool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @bool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int Num1 =10, Num2 = 90;
+            ////bool No2 = Num1 <= 9 && Num2 <= 100;
+            //bool No2 = Num1 <=9 || Num2 <= 100;
+            //Console.WriteLine(No2);
+            
+           
+
+            Console.Write("输入语文成绩：");
+            int Chn = Convert.ToInt32(Console.ReadLine());
+            Console.Write("输入英语成绩：");
+            int Eng = Convert.ToInt32(Console.ReadLine());
+            bool result = Chn >= 90 || Eng >= 90;
+            Console.WriteLine(result);
+            
+            Console.ReadKey();
+        }
+    }
+}
