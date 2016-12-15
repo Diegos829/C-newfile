51d7ff055c13417e2f66c65db25e0b77aaa8cd1e
Diegos829 <django@163.com>
2016-12-16 07:43:00 +0800

    判断语句的基础使用

--- bool/Program.cs
+++ bool/Program.cs
@@ -25,11 +25,17 @@ namespace @bool
             //bool result2 = Chn >= 90 || Eng >= 90;
             //Console.WriteLine("\"我的成绩都超过了90分\"，是真的吗？{0}，\"其中有一门成绩超过了90分是真的吗？\"{1}",result,result2);
 
-            Console.WriteLine("输入一个年份，看是否闰年。");
-            int year = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("输入一个年份，看是否闰年。");
+            //int year = Convert.ToInt32(Console.ReadLine());
 
-            bool result = (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
-            Console.WriteLine("这一年{0}",result);
+            //bool result = (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
+            //Console.WriteLine("这一年{0}",result);
+            //
+
+            int Num1 = 6, Num2 = 11; Num1+=5;
+            //bool result = ++Num1>50 && ++Num2 > 3;
+            bool result = ++Num1 < 50 || ++Num2 > 3;
+            Console.WriteLine("Num1={0},Num2={1}",Num1,Num2);
             Console.ReadKey();
         }
     }
---
+++ ifd/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("来把你的工资填写进来看一下：");
+            //string pay = Console.ReadLine();
+            //int mypay = Convert.ToInt32(pay);
+            //if (mypay > 5000)
+            //    Console.WriteLine("恭喜你进入了穷人阶级！");////if只带这句
+            //Console.WriteLine("哎哟，你还在生死线上徘徊，还要加油啊！");
+
+
+            //Console.WriteLine("来把你的工资填写进来看一下：");
+            //int mypay = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("再把年纪填进来看一下：");
+            //int myage = Convert.ToInt32(Console.ReadLine());
+            //if ((mypay>=6000&&myage>=35)||(mypay==5500&&myage>=30))
+            //{
+            //    Console.WriteLine("恭喜你脱离了生死线，进入了穷人阶级！");
+            //}
+
+
+            Console.Write("输入用户名：");
+            string UserName = Console.ReadLine();
+            Console.Write("输入密码：");
+            string PassWord = Console.ReadLine();
+            if (UserName == "admin" && PassWord == "password")
+            {
+                Console.WriteLine("输入正确，什么也不用打开");
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
